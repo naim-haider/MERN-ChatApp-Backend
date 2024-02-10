c202b1ba600d812ad120107f8ceb35008aff9576
naim-haider <naimhaider76@gmail.com>
2024-02-10 21:00:50 +0530

    origin change

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json()); // to accept json data
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT"],
     credentials: true,
   })
